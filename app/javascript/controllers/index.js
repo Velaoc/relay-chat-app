c4ae7090f668516a36093cf33d6ed32818b04afa
Vela <velaoc@proton.me>
2026-08-11 14:05:57 -0600

    Register chat Stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,7 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
+// Import and register all your controllers from the importmap under controllers/*controller.js
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import ChatController from "controllers/chat_controller"
+application.register("chat", ChatController)
+
+import HelloController from "controllers/hello_controller"
+application.register("hello", HelloController)
